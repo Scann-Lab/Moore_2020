0d502d3dc78b7f2aa6c79579b67de46e2a5cc860
Kunath <jeffreykunath@ufl.edu>
2021-11-12 15:45:57 -0500

    DSP_fMRI_11003

--- DSP_PsychoPy/DSP_Behavioral_Only_Encoding_lastrun.py
+++ DSP_PsychoPy/DSP_Behavioral_Only_Encoding_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on November 12, 2021, at 10:43
+    on November 12, 2021, at 15:12
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
