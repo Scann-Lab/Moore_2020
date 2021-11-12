3dc1b057e6d90e784ebafb42fbcb3a982e133e37
Perez <elianyperez@ufl.edu>
2021-11-12 11:14:08 -0500

    11002

--- DSP_PsychoPy/DSP_Bold_lastrun.py
+++ DSP_PsychoPy/DSP_Bold_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on October 29, 2021, at 12:30
+    on November 12, 2021, at 10:15
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
