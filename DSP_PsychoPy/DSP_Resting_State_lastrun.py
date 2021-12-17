1c94e10870edea1b6a804a24cd550032991ae263
Kunath <jeffreykunath@ufl.edu>
2021-12-17 15:19:33 -0500

    DSP_fMRI_12008

--- DSP_PsychoPy/DSP_Resting_State_lastrun.py
+++ DSP_PsychoPy/DSP_Resting_State_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on December 10, 2021, at 11:38
+    on December 16, 2021, at 14:13
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
