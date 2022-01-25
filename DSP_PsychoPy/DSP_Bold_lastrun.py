09e8ee6a8453e9a072c9538efd6620c7bfce4715
Kunath <jeffreykunath@ufl.edu>
2022-01-25 10:43:42 -0500

    DPS_fMRI11004, DSP_fMRI_12009

--- DSP_PsychoPy/DSP_Bold_lastrun.py
+++ DSP_PsychoPy/DSP_Bold_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on January 21, 2022, at 12:10
+    on January 24, 2022, at 14:45
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
