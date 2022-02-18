c0e57288015870babbba1e1a8c6ac9ebbcaea8e5
Kunath <jeffreykunath@ufl.edu>
2022-02-18 13:01:53 -0500

    DSP_fMRI_11007

--- LocalizerCode/localizer_lastrun.py
+++ LocalizerCode/localizer_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on February 14, 2022, at 14:45
+    on February 17, 2022, at 15:34
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
