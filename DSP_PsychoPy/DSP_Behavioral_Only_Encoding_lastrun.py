997564c5e8a27eb1571d15606991055ab420e3aa
Kunath <jeffreykunath@ufl.edu>
2022-02-21 10:09:55 -0500

    DSP_fMRI_11008

--- DSP_PsychoPy/DSP_Behavioral_Only_Encoding_lastrun.py
+++ DSP_PsychoPy/DSP_Behavioral_Only_Encoding_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on February 17, 2022, at 16:42
+    on February 18, 2022, at 16:44
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
--- DSP_PsychoPy/DSP_Bold_lastrun.py
+++ DSP_PsychoPy/DSP_Bold_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on February 17, 2022, at 16:13
+    on February 18, 2022, at 16:11
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
