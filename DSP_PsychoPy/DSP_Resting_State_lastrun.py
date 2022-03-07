96d6efc610608cb9f915d4bfacf593d96d2dbe88
Kunath <jeffreykunath@ufl.edu>
2022-03-07 13:37:38 -0500

    dspfmri_11012

--- DSP_PsychoPy/DSP_Resting_State_lastrun.py
+++ DSP_PsychoPy/DSP_Resting_State_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on March 03, 2022, at 13:54
+    on March 07, 2022, at 11:00
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
--- LocalizerCode/localizer_lastrun.py
+++ LocalizerCode/localizer_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on March 03, 2022, at 14:22
+    on March 07, 2022, at 11:37
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
