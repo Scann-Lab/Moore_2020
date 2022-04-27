6642fbce5288157977712725cd51c8435759435f
Kunath <jeffreykunath@ufl.edu>
2022-04-27 09:03:04 -0400

    dspfmri_22009

--- DSP_PsychoPy/DSP_Encoding_Refresher_lastrun.py
+++ DSP_PsychoPy/DSP_Encoding_Refresher_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on April 21, 2022, at 13:29
+    on April 26, 2022, at 15:09
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
