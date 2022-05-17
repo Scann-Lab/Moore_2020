5c9f34c4275d4a713a61ed8a31ba4b85237d3a1c
Kunath <your_email@domainname.com>
2022-05-17 13:06:39 -0400

    11014, 12015

--- LocalizerCode/localizer_lastrun.py
+++ LocalizerCode/localizer_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on April 27, 2022, at 11:47
+    on May 12, 2022, at 14:06
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
