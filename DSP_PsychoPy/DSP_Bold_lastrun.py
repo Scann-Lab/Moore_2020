fe4406f4683f8483d8fb94408d84c8e8fee9a433
Kunath <your_email@domainname.com>
2022-06-02 13:29:57 -0400

    dspfmri_11015

--- DSP_PsychoPy/DSP_Bold_lastrun.py
+++ DSP_PsychoPy/DSP_Bold_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on May 25, 2022, at 12:57
+    on May 26, 2022, at 12:44
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
