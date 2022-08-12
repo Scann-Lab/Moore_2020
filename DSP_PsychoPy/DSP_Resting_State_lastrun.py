da64f7455ea0412576dff66b69770eb455e6e2d8
Kunath <your_email@domainname.com>
2022-08-12 14:47:58 -0400

    dspfmri_22011

--- DSP_PsychoPy/DSP_Resting_State_lastrun.py
+++ DSP_PsychoPy/DSP_Resting_State_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on August 11, 2022, at 11:17
+    on August 12, 2022, at 11:59
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
