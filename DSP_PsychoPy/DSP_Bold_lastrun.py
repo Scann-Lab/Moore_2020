ba99d15df906f5adb0e37e9587d058d66485b35a
Kunath <your_email@domainname.com>
2023-04-20 12:36:55 -0400

    dspfmri_22054

--- DSP_PsychoPy/DSP_Bold_lastrun.py
+++ DSP_PsychoPy/DSP_Bold_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2020.2.10),
-    on April 05, 2023, at 13:39
+    on April 20, 2023, at 11:19
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
--- DSP_PsychoPy/DSP_Resting_State_lastrun.py
+++ DSP_PsychoPy/DSP_Resting_State_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on April 05, 2023, at 12:25
+    on April 20, 2023, at 09:57
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
