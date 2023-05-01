913c9cacb59348a025592070bdbf6fa8ff6d6e73
Kunath <your_email@domainname.com>
2023-05-01 16:37:15 -0400

    dspfmri_22055

--- LocalizerCode/localizer_lastrun.py
+++ LocalizerCode/localizer_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.3),
-    on April 20, 2023, at 10:38
+    on May 01, 2023, at 14:50
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
